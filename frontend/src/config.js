75e9e2ca7f23abf386f7878d75c3c9cbca629891
MateuszC16 <matitek1234@gmail.com>
2025-05-17 01:18:21 +0200

    merged

---
+++ frontend/src/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://tg4n8lh6-8000.euw.devtunnels.ms';
+
+export const ENDPOINTS = {
+  GET_ALL_DAYS: `${API_URL}/api/days/by-user/1`,
+  CREATE_DAY: `${API_URL}/api/days/`,
+  GET_USER: `${API_URL}/api/users/1`,
+  // tutaj możesz dodać więcej endpointów w przyszłości
+};
