89bc4b92601d8186bd236e79d555e89fe05c7301
Szymon Kazmierczak <251546@edu.p.lodz.pl>
2025-05-16 16:48:57 +0200

    logowanie bez sprawdzania

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -7,20 +7,6 @@ function HomePage() {
       <div className="welcome-content">
         <h1>Witaj w aplikacji Wellness</h1>
         <p>Twoje miejsce do dbania o zdrowie psychiczne</p>
-        <div className="features">
-          <div className="feature pink">
-            <h3>Dziennik</h3>
-            <p>Zapisuj swoje myśli i emocje</p>
-          </div>
-          <div className="feature orange">
-            <h3>Medytacja</h3>
-            <p>Ćwiczenia mindfulness</p>
-          </div>
-          <div className="feature green">
-            <h3>Ćwiczenia</h3>
-            <p>Aktywności wspierające dobre samopoczucie</p>
-          </div>
-        </div>
       </div>
     </div>
   );
