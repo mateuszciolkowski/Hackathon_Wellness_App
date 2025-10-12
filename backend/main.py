261cb6ce3f85e8b1e04d34bacab86f2a57cfe530
MateuszC16 <matitek1234@gmail.com>
2025-10-12 20:58:34 +0200

    added cors for server

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000/",
     "http://127.0.0.1:3000/",
+    
     "*"  # UWAGA: pozwala na wszystko (OK w dev, nie w produkcji)
 ]
 
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     '/api',
     createProxyMiddleware({
     //   target: 'https://tg4n8lh6-8000.euw.devtunnels.ms:8000',
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8001',
       changeOrigin: true,
       secure: false,
       pathRewrite: {
